12fa93e69c30dfac6256592a0c0e838da7d26dae
Bernardino Guerrero Ibarra <bguerrero@nearsoft.com>
2020-02-08 21:55:31 -0600

    starting 17th day

---
+++ SwiftUIChallengeD17/SwiftUIChallengeD17/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUIChallengeD17
+//
+//  Created by Bernardino Guerrero on 08/02/20.
+//  Copyright © 2020 Bernardino Guerrero. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
